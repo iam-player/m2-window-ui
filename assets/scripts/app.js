40b43816790a270233f0fef3775070357f543fdf
iam-player <iampl4y3r@gmail.com>
2023-07-10 21:23:51 +0200

    disabled drag & drop of icons to retain hover & click effects

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,15 @@ const iconsLeft = ['is', 'casino', 'magazyn', 'wyprawa', 'biolog', 'bs', 'top-gr
 const iconsRight = ['battlepass', 'dailyreward', 'sklep', 'polowanie', 'kolekcjoner', 'zmiana-eq', 'top-gildie', 'poli-usun', 'autobuff', 'bonusy24', 'patch-notes', 'wiki', 'snake'];
 const iconsPath = './assets/images/sidebar/icons/';
 
+
+//insert images into sidebar grid
 for (let i = 0; i < iconsLeft.length; i++){
     leftColumn.innerHTML += `<img class="sidebarIcon" src="${iconsPath}${iconsLeft[i]}/${iconsLeft[i]}_norm.png">`;
     rightColumn.innerHTML += `<img class="sidebarIcon" src="${iconsPath}${iconsRight[i]}/${iconsRight[i]}_norm.png">`;
+}
+
+//disable dragging
+const imgs = document.getElementsByTagName('img');
+for(let j = 0; j < imgs.length; j++ ) {
+    imgs[j].setAttribute("draggable", "false")
 }
